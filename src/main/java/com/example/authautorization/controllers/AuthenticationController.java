89b315f8e7d89b01188e73d3d73ae2a534a46708
Alan Nicolas <alanicolax@gmail.com>
2025-08-17 14:38:20 -0400

    configurando o filtro do jwt e services

---
+++ src/main/java/com/example/authautorization/controllers/AuthenticationController.java
@@ -0,0 +1,1 @@
+package com.example.authautorization.controllers;import org.springframework.web.bind.annotation.RequestMapping;import org.springframework.web.bind.annotation.RestController;@RestController@RequestMapping("/api/auth")public class AuthenticationController {}
