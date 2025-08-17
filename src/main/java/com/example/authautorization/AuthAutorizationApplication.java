605456b6d14aa5a217d18310049469b5f106c5c0
Alan Nicolas <alanicolax@gmail.com>
2025-08-17 10:43:26 -0400

    primeiro commit

---
+++ src/main/java/com/example/authautorization/AuthAutorizationApplication.java
@@ -0,0 +1,13 @@
+package com.example.authautorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthAutorizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthAutorizationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/authautorization/AuthAutorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authautorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthAutorizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
